681b467e60894af2c94a565f54013baec1885c69
Kuchka-pili <utrajber@gmail.com>
2026-03-08 20:25:35 +0800

    Готова первая версия приложения (базовый тренажер)

--- Typing-fast/Typing-fast/Form1.Designer.cs
+++ Typing-fast/Typing-fast/Form1.Designer.cs
@@ -29,25 +29,94 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 298);
+            this.label1.Location = new System.Drawing.Point(9, 163);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(156, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Создатель: Трайбер Валерия";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(173, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Приложение для набора текста";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Pink;
+            this.textBox1.Location = new System.Drawing.Point(12, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(497, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Pink;
+            this.button1.Location = new System.Drawing.Point(326, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Новый текст";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ваша скорость:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Нажмите на \"Новый текст\"";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(102, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(10, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = " ";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(521, 320);
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(525, 206);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Приложение для набора текста";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,6 +125,12 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- Typing-fast/Typing-fast/Form1.cs
+++ Typing-fast/Typing-fast/Form1.cs
@@ -12,9 +12,55 @@ namespace Typing_fast
 {
     public partial class Form1 : Form
     {
+        private DateTime startTime;
+        private bool isStarted = false;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] texts = new string[]
+   {
+        "кот собака дом солнце",
+        "мама мыла раму",
+        "зеленый чай вкусный",
+        "я люблю программировать",
+        "сегодня хорошая погода",
+        "быстрый коричневый лис",
+        "привет как дела"
+   };
+            Random rand = new Random();
+            int index = rand.Next(texts.Length);
+            label4.Text = texts[index];
+            textBox1.Clear();
+            isStarted = false;
+            label5.Text = "0";
+            textBox1.Focus();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (!isStarted && textBox1.Text.Length > 0)
+            {
+                startTime = DateTime.Now;
+                isStarted = true;
+            }
+            if (isStarted)
+            {
+                double minutes = (DateTime.Now - startTime).TotalMinutes;
+                if (minutes > 0)
+                {
+                    int cpm = (int)(textBox1.Text.Length / minutes); 
+                    label5.Text = cpm.ToString(); 
+                }
+            }
+            if (textBox1.Text == label4.Text)
+            {
+                MessageBox.Show("Молодец! Ты справился!", "Победа");
+                isStarted = false;
+            }
         }
     }
 }
